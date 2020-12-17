919e9241756fea454202c350b76183e98141deb6
gunjan <pandya.gunjan@gmail.com>
2020-12-16 23:18:29 -0500

    changes

--- src/main/java/controllers/AppRunner.java
+++ src/main/java/controllers/AppRunner.java
@@ -1,12 +1,14 @@
 package controllers;
 
 import entities.CD;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import services.CDService;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 //@RestController
+@SpringBootApplication
 public class AppRunner  {
 
     public static void main(String[] args) {
